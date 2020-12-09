65f875fd93db5713133e6f44833c1f53ac39636b
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2020-12-09 14:23:55 -0500

    updates

--- src/components/UnionSearch.vue
+++ src/components/UnionSearch.vue
@@ -3,22 +3,20 @@
 
   <div class="container is-fluid py-5 px-3">
 
-    <div class="columns is-desktop">
-      <div class="column is-two-thirds-desktop">
+    
         <div class="field">
       <p class="control is-expanded">
-        <input v-model="searchInput" @keyup.enter="submitSearch" class="input" type="text" placeholder="Enter titles, creators, or subjects">
+        <input v-model="searchInput" @keyup.enter="submitSearch" class="input is-large" type="text" placeholder="Enter titles, creators, or subjects">
       </p>
       </div>
-      </div>
-      <div class="column">
+
         <div class="field has-addons">
-          <p class="control">
-        <span class="select">
+          <p class="control is-expanded">
+        <span class="select is-fullwidth">
           <select name="search-type" id="" v-model="searchType">
-            <option value="both">Both</option>
-            <option value="dc">Digital Collections</option>
-            <option value="ia">Internet Archive</option>
+            <option value="both">Search both repositories</option>
+            <option value="dc">Digital Collections only</option>
+            <option value="ia">Internet Archive only</option>
           </select>
         </span>
       </p>
@@ -28,8 +26,6 @@
         </a>
       </p>
         </div>
-      </div>
-      </div>
     
 
 
